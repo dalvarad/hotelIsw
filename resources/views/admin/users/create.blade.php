5c5245d1b9051a977845fb5ff2a52929a005f1d3
Diego Alvarado <Diego Alvarado>
2017-07-05 13:46:00 -0500

    arreglo de vistas

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -5,37 +5,68 @@
 @section('contenido')
 
 	{!! Form::open(['route' => 'admin.usuarios.store', 'method' => 'POST']) !!}
-
-		<div class="form-group"> 
-			{!! Form::label('nombre_usuario', 'Nombre de Usuario') !!}
-			{!! Form::text('nombre_usuario', null, ['class' => 'form-control', 'placeholder' => 'Ramón Ramírez Roman', 'required']) !!}
+<div class="container">
+	<div class="row">
+		<div class="col_md_4">
 		</div>
+		<div class="col_md_4">
+			<div class="row">
+				<div class="col_md_4">
+					
 
-		<div class="form-group">
-			{!! Form::label('rut_usuario', 'Rut Usuario') !!}
-			{!! Form::text('rut_usuario', null, ['class' => 'form-control', 'placeholder' => '1.111.111-1', 'required']) !!}
+					</div>
+   				</div>
+			<div class="col_md_4">
+			</div>
 		</div>
-
-		<div class="form-group">
-			{!! Form::label('usuario', 'Nick Usuario') !!}
-			{!! Form::text('usuario', null, ['class' => 'form-control', 'placeholder' => 'Rarami', 'required']) !!}
-		</div>
-
-		<div class="form-group">
-			{!! Form::label('pass', 'Contraseña Usuario') !!}
-			{!! Form::password('pass',['class' => 'form-control', 'placeholder' => '********', 'required']) !!}
-		</div>
-
-		<div class="form-group">
-			{!! Form::label('tipo', 'Tipo de Usuario') !!}
-			{!! Form::select('tipo', ['' => 'Seleccione un nivel', 'administrador', 'recepcionista'], null, ['class' => 'form-control',  'required']) !!}
-		</div>
-
-		<div class="form-group">
-			{!! Form::submit('Registrar', ['class' => 'btn btn-primary']) !!}
-		</div>
+	</div>
+</div>
 
 
+<div class="container" xmlns="http://www.w3.org/1999/html">
+    <div class="row">
+        <div class="col-md-3">
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Registrar Usuario
+                    </div>
+                    <div class="panel-body">
+                       <div class="form-group"> 
+						{!! Form::label('nombre_usuario', 'Nombre de Usuario') !!}
+						{!! Form::text('nombre_usuario', null, ['class' => 'form-control', 'placeholder' => 'Ramón Ramírez Roman', 'required']) !!}
+					</div>
+
+					<div class="form-group">
+						{!! Form::label('rut_usuario', 'Rut Usuario') !!}
+						{!! Form::text('rut_usuario', null, ['class' => 'form-control', 'placeholder' => '1.111.111-1', 'required']) !!}
+					</div>
+
+					<div class="form-group">
+						{!! Form::label('usuario', 'Nick Usuario') !!}
+						{!! Form::text('usuario', null, ['class' => 'form-control', 'placeholder' => 'Rarami', 'required']) !!}
+					</div>
+
+					<div class="form-group">
+						{!! Form::label('pass', 'Contraseña Usuario') !!}
+						{!! Form::password('pass',['class' => 'form-control', 'placeholder' => '********', 'required']) !!}
+					</div>
+
+					<div class="form-group">
+						{!! Form::label('tipo', 'Tipo de Usuario') !!}
+						{!! Form::select('tipo', ['' => 'Seleccione un nivel', 'administrador', 'recepcionista'], null, ['class' => 'form-control',  'required']) !!}
+					</div>
+
+					<div class="form-group">
+						{!! Form::submit('Registrar', ['class' => 'btn btn-primary']) !!}
+					</div>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
 
 	{!! Form::close() !!}
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,21 +10,38 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 </head>
 <body>
-    <header>
-    <div class="menu_bar">
-        <a href="#" class="bt-menu"><span class="icon-cross"></span>Menu</a>
-    </div>
-        <nav>
-            <ul>
-                <li><a href="{{url('/')}}"><span class="icon-home3"></span>Inicio</a></li>
-                <li><a href="{{url('sucursales')}}"><span class="icon-briefcase"></span>Habitaciones</a></li>
-                <li><a href="{{url('hotel')}}"><span class="icon-earth"></span>¿Quiénes somos?</a></li>
-                <li><a href="#"><span class="icon-mail3"></span>Contactos</a></li>
-                <li><a href="#"><span class="icon-rocket"></span>Log In</a></li>
-            </ul>
+  
+        <nav class="navbar navbar-default navbar-static-top">     
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toogle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">     
+                                                                                                                               
+                        <span class="sr-only"> Este boton despliega la barra de navegacion</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Menu</a>
+                </div>
+                
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav navbar-center">
+                        <li><a href="{{url('/')}}"><span class="icon-home3"></span>Inicio</a></li>
+                        <li><a href="{{url('sucursales')}}"><span class="icon-briefcase"></span>Habitaciones</a></li>
+                        <li><a href="{{url('hotel')}}"><span class="icon-earth"></span>¿Quiénes somos?</a></li>
+                        
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#"><span class="icon-rocket"></span>Log In</a></li>
+                        <li><a href="#"><span class="icon-mail3"></span>Contactos</a></li>
+                    </ul>
+                </div>
+            </div>
         </nav>
-        @yield('contenido')
-    </header>
+         @yield('contenido')
+
+
+
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script rel="javascript"  type="text/javascript" src="estilos/barra/menu.js"></script>
 </body>
