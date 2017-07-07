794bc31ecca1bad62d64e394bbfaad119a21b69e
Diego Alvarado <Diego Alvarado>
2017-07-06 20:44:56 -0500

    rut validador 3

--- app/Http/Controllers/UsuariosController.php
+++ app/Http/Controllers/UsuariosController.php
@@ -9,8 +9,6 @@ use Illuminate\Support\Facades\Session;
 use App\Http\Requests\UsuarioRequest;
 use Freshwork\ChileanBundle\Rut;
 
-
-
 class UsuariosController extends Controller
 {
 
@@ -20,12 +18,10 @@ class UsuariosController extends Controller
         return view('admin.usuarios.index')->with('usuarios', $usuarios);
     }
 
-
     public function create()
     {
         return view('admin.usuarios.create');
     }
-
 
     public function store(UsuarioRequest $request)
     {
@@ -33,39 +29,44 @@ class UsuariosController extends Controller
         $user->pass = bcrypt($request->pass);
         $user->rut_usuario = RUT::parse($request->rut_usuario)->format(RUT::FORMAT_WITH_DASH);
         $user->save();
+        
         Session::flash('message_success', "Se ha registrado el usuario $user->nombre_usuario Exitosamente!");
         return redirect(route('admin.usuarios.index'));
     }
-
 
     public function show($id)
     {
         //
     }
 
-
     public function edit($id){
         $user = Usuario::find($id);
         return view('admin.usuarios.edit')->with('user', $user);
-    }  
-
+    }
 
     public function update(Request $request, $id){
         $user = Usuario::find($id);
-        
+    
+        $user->nombre_usuario = $request->nombre_usuario;
         $this->validate($request,[
             'rut_usuario' => 'cl_rut'
         ]);
+
+        $user->usuario = $request->usuario;
+        $user->tipo = $request->tipo;
         $user->save();
+        
         Session::flash('message_success', "Se ha modificado el usuario $user->nombre_usuario Exitosamente!");
         return redirect(route('admin.usuarios.index'));
+     
     }
-
 
     public function destroy($id){
         $user = Usuario::find($id);
-        $user->delete();    
+        $user->delete();  
+
         Session::flash('message_danger', "Se ha eliminado el usuario $user->nombre_usuario Exitosamente!");
         return redirect(route('admin.usuarios.index'));
     }
+
 }
